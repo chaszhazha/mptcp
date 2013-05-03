05763ffb04ef7fb765424f9818969cc4623ada53
Charles <charles.chuckles1990@gmail.com>
2013-05-03 15:02:48 -0400

    ...

--- test_topo.py
+++ test_topo.py
@@ -1,14 +1,15 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.link import TCLink
+from mininet.node import CPULimitedHost
 
 class TestTopo(Topo):
     def __init__(self):
         Topo.__init__(self)
         leftHost = self.addHost('h1')
         rightHost = self.addHost('h2')
-        leftSwitch = self.addSwitch('s1')
-        rightSwitch = self.addSwitch('s2')
+        leftSwitch = self.addSwitch('h3')
+        rightSwitch = self.addSwitch('h4')
 
         self.addLink(leftHost,leftSwitch, bw=100)
         self.addLink(leftSwitch, rightSwitch, bw=100)
