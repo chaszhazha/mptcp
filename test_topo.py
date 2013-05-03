8bb735ae454a426c0b89275d952d8f145abfc112
Charles <charles.chuckles1990@gmail.com>
2013-05-02 22:53:04 -0400

    ...

--- test_topo.py
+++ test_topo.py
@@ -5,8 +5,8 @@ class TestTopo(Topo):
         Topo.__init__(self)
         leftHost = self.addHost('h1')
         rightHost = self.addHost('h2')
-        leftswitch = self.addSwitch('s1')
-        rightswithc = self.addSwitch('s2')
+        leftSwitch = self.addSwitch('s1')
+        rightSwitch = self.addSwitch('s2')
 
         self.addLink(leftHost,leftSwitch)
         self.addLink(leftSwitch, rightSwitch)
