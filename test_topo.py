3221c2bead3588ee1e883bca4565d02452d7b577
Charles <charles.chuckles1990@gmail.com>
2013-05-03 20:51:33 -0400

    ...

--- test_topo.py
+++ test_topo.py
@@ -9,8 +9,8 @@ class TestTopo(Topo):
         Topo.__init__(self)
         leftHost = self.addHost('h1')
         rightHost = self.addHost('h2')
-        leftSwitch = self.addHost('h3')
-        rightSwitch = self.addHost('h4')
+        leftSwitch = self.addSwitch('s1')
+        rightSwitch = self.addSwitch('s2')
         
 
         link1 = self.addLink(leftHost,leftSwitch)
@@ -23,11 +23,9 @@ topos = {'testTopo': (lambda :TestTopo() )}
 if __name__ == '__main__':
     net = Mininet(topo = TestTopo())
     net.start()
-    h3 = net.getNodeByName('h3')
-    print h3.cmd('sysctl -w net.ipv4.ip_forward=1')
-    h4 = net.getNodeByName('h4')
-    print h4.cmd('sysctl -w net.ipv4.ip_forward=1')
-    print h3.cmd('ping -c 1 ', h4.IP())
+    s1 = net.getNodeById('s1')
+    s2 = net.getNodeById('s2')
+    print net.hosts
     sleep(5)
 
     net.stop()
